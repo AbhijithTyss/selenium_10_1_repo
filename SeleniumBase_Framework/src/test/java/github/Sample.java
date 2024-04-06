a83cdcd235f39500a879617a329195b47988cbbc
AbhijithTyss <145775712+AbhijithTyss@users.noreply.github.com>
2024-04-06 09:37:40 +0530

    Update Sample.java

--- SeleniumBase_Framework/src/test/java/github/Sample.java
+++ SeleniumBase_Framework/src/test/java/github/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Sample class added to github");
+		System.out.println("Sample class edited in github");
 	}
 
 }
