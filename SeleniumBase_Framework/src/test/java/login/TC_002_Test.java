0de9efea60cf630d63073c2ee7dbb29782d1a3e9
AbhijithTyss <145775712+AbhijithTyss@users.noreply.github.com>
2024-04-06 09:40:21 +0530

    Update TC_002_Test.java

--- SeleniumBase_Framework/src/test/java/login/TC_002_Test.java
+++ SeleniumBase_Framework/src/test/java/login/TC_002_Test.java
@@ -3,5 +3,5 @@ package login;
 import com.tyss.ecommerce.genericutility.BaseClass;
 
 public class TC_002_Test extends BaseClass{
-
+      System.out.println("TC_002_Test class edited to github");
 }
